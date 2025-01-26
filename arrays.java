921530d5e5bccc5f69d8d6f56338a2d96d108d24
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-01-26 23:37:30 +0530

    first commit

---
+++ arrays.java
@@ -0,0 +1,27 @@
+
+public class arrays {
+//second largest 
+    // public int getSecondLargest(int[] arr) {
+    //     // Code Here
+    //     int n = arr.length;
+    //     int large = Integer.MIN_VALUE;
+    //     int slarge = Integer.MIN_VALUE;
+        
+    //     if(n<2) return -1;
+        
+    //     for(int i = 0; i < n; i++){
+    //         if(arr[i] > large){
+    //             slarge = large;
+    //             large = arr[i];
+    //         }else if(arr[i]< large && slarge < arr[i]){
+    //             slarge = arr[i];
+    //         }
+            
+    //     }
+    //     if(slarge == Integer.MIN_VALUE) return -1;
+    //     return slarge;
+    // }
+
+
+    
+}
